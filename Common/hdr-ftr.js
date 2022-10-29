d9a29835ba22291d43bbf096cfd35c1a8c498e1a
wu-jldeyoung <97631463+wu-jldeyoung@users.noreply.github.com>
2022-10-29 02:35:06 -0700

    Added skills list, started accordion

--- Common/hdr-ftr.js
+++ Common/hdr-ftr.js
@@ -24,6 +24,15 @@ let appFooter = `
 document.getElementById("app-header").innerHTML = appHeader;
 document.getElementById("app-footer").innerHTML = appFooter;
 
+function hide_show(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+	x.className += " w3-show";
+  } else {
+	x.className = x.className.replace(" w3-show", "");
+  }
+}
+
 /*
 // Get the Sidebar
 var mySidebar = document.getElementById("mySidebar");
